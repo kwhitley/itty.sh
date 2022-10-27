214df7a97ad5da497304a1e0b495724a9ddf6253
Kevin Whitley <krwhitley@gmail.com>
2022-10-27 00:39:16 -0500

    staging some stuff for later

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,16 @@
 <script>
-  import { randomItem } from 'supergeneric/randomItem'
-  import SearchInput from '~/components/SearchInput.svelte'
   import KeyLength from '~/components/KeyLength.svelte'
   import TTL from '~/components/TTL.svelte'
-  import { api } from '~/ity.sh/api'
   import Page from '~/layout/Page.svelte'
-  import { onInterval } from '~/utils/onInterval'
   import Intro from './Intro.svelte'
   import Preview from './Preview.svelte'
-  import { keyLength, textTTL } from '~/stores'
+  import SimpleInput from './SimpleInput.svelte'
+  import { textTTL } from '~/stores'
 
-  const PLACEHOLDER_SHUFFLE_SPEED = 2000
-
-  let value = ''
-  let placeholderOptions = [
-    'enter a link',
-    'enter some text',
-    'paste an image',
-    // 'write a function*',
-    'drag a file*',
-  ]
-  let placeholder = placeholderOptions[0]
   let id = ''
   let submitting = false
 
-  // shuffle function
-  const shuffle = () => {
-    let next = placeholder
-
-    while (next === placeholder) {
-      next = randomItem(placeholderOptions)
-    }
-
-    placeholder = next
-  }
-
-  const { start, stop } = onInterval(shuffle, PLACEHOLDER_SHUFFLE_SPEED)
-
-  // REACTIVE BITS
-  $: disabled = submitting || !value
-  $: if (value) {
-    stop()
-  } else {
-    start()
-  }
-
-  const detectType = payload => {
-    if (typeof payload !== 'string') return ''
-
-    if (payload.match(/(<html>)|(<\/?\w+>)/gi)) return 'text/html'
-
-    return ''
-  }
-
-  // SAVE PAYLOAD
-  const save = async (payload, as = '') => {
-    submitting = true
-    value = ''
-    stop() // stop placeholder
-    placeholder = 'generating...'
-
-    // auto-detect type if not explicity passed
-    if (!as) {
-      as = detectType(payload)
-    }
-
-    await api.post(`/create?length=${$keyLength}&as=${as}&ttl=${$textTTL}`, payload)
-            .then(response => {
-              id = response.key
-            })
-            .catch(err => {
-              submitting = false
-              console.error('ERROR: There was an error creating a link.', err.stack)
-            })
-
-    shuffle()
-    start() // resume placeholder shuffle
-    submitting = false
-  }
+  $: console.log('submitting has changed', submitting)
 </script>
 
 <Page>
@@ -86,15 +19,15 @@
     <TTL />
 
     <section class="form">
-      <SearchInput
-        placeholder={placeholder}
-        bind:value={value}
-        on:file={e => save(e.detail)}
-        on:submit={() => save(value)}
-        disabled={disabled}
+      <SimpleInput
+        bind:id={id}
+        bind:submitting={submitting}
         />
 
-      <Preview id={id} submitting={submitting} />
+      <Preview
+        id={id}
+        submitting={submitting}
+        />
     </section>
 
     <Intro />
@@ -119,14 +52,5 @@
     flex-flow: column;
     gap: 0.3em;
     margin-bottom: 0.1em;
-  }
-
-  .editor-container {
-    font-size: 0.5em;
-  }
-
-  label {
-    font-size: 1.3rem;
-    margin-bottom: 0em;
   }
 </style>
---
+++ src/routes/SimpleInput.svelte
@@ -0,0 +1,94 @@
+<script>
+  import { randomItem } from 'supergeneric/randomItem'
+  import SearchInput from '~/components/SearchInput.svelte'
+  import { api } from '~/ity.sh/api'
+  import { keyLength, textTTL } from '~/stores'
+  import { onInterval } from '~/utils/onInterval'
+
+  // CONSTANTS
+  const PLACEHOLDER_SHUFFLE_SPEED = 2000
+
+  // external variables
+  export let id = ''
+  export let submitting = false
+
+  let value = ''
+  let placeholderOptions = [
+    'enter a link',
+    'enter some text',
+    'paste an image',
+    // 'write a function*',
+    'drag a file*',
+  ]
+  let placeholder = placeholderOptions[0]
+
+  // shuffle placeholder
+  const shuffle = () => {
+    let next = placeholder
+
+    while (next === placeholder) {
+      next = randomItem(placeholderOptions)
+    }
+
+    placeholder = next
+  }
+
+  // register start/stop functions for the placeholder shuffle
+  const { start, stop } = onInterval(shuffle, PLACEHOLDER_SHUFFLE_SPEED)
+
+  // REACTIVE BITS
+  $: disabled = submitting || !value
+  $: if (value) {
+    stop()
+  } else {
+    start()
+  }
+
+  const detectType = payload => {
+    if (typeof payload !== 'string') return ''
+
+    if (payload.match(/(<html>)|(<\/?\w+>)/gi)) return 'text/html'
+
+    return ''
+  }
+
+  // SAVE PAYLOAD
+  const save = async (payload, as = '') => {
+    submitting = true
+    value = ''
+    stop() // stop placeholder
+    placeholder = 'generating...'
+
+    // auto-detect type if not explicity passed
+    if (!as) {
+      as = detectType(payload)
+    }
+
+    await api.post(`/create?length=${$keyLength}&as=${as}&ttl=${$textTTL}`, payload)
+            .then(response => {
+              id = response.key
+            })
+            .catch(err => {
+              submitting = false
+              console.error('ERROR: There was an error creating a link.', err.stack)
+            })
+
+    shuffle()
+    start() // resume placeholder shuffle
+    submitting = false
+  }
+</script>
+
+<!-- MARKUP -->
+<SearchInput
+  placeholder={placeholder}
+  bind:value={value}
+  on:file={e => save(e.detail)}
+  on:submit={() => save(value)}
+  disabled={disabled}
+  />
+
+<!-- STYLES -->
+<style lang="scss">
+
+</style>
