5b5b2db7452c921f0e1cbd9d68928f660dc15805
Kevin Whitley <krwhitley@gmail.com>
2022-11-04 01:44:17 -0500

    locking in definition lists

---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<!-- MARKUP -->
+<h1>Stats</h1>
+
+<dl>
+  <dt>Waitlist Users</dt>
+  <dd>Foo</dd>
+
+  <dt>Waitlist Users</dt>
+  <dd>Foo</dd>
+
+  <dt>Waitlist Users</dt>
+  <dd>Foo</dd>
+
+  <dt>Waitlist Users</dt>
+  <dd>Foo</dd>
+</dl>
+
+<style lang="scss">
+
+</style>
